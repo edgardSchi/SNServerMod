568c936da7f75d31d164a5cd384f86f5c24ff427
Niphoria <niphoria.chan@gmail.com>
2019-10-31 14:39:53 +0100

    puschi

--- SilensNox/scripts/4_world/entities/Vehicles/vehicleskey.c
+++ SilensNox/scripts/4_world/entities/Vehicles/vehicleskey.c
@@ -1,3 +1,4 @@
+// This is modded by Niphoria 
 modded class CarScript
 {
 	override void EEItemDetached(EntityAI item, string slot_name)
@@ -16,7 +17,7 @@ modded class CarScript
 			if ( EngineIsOn() )
 			{
 				//EngineBelt
-				if ( slot_name == "GlowPlug" || slot_name == "SparkPlug" || slot_name == "CarBattery" || slot_name == "TruckBattery" || slot_name == "SNWolgaK_1" || slot_name == "SNWolgaK_2" || slot_name == "SNWolgaK_3" || slot_name == "SNWolgaK_4" || slot_name == "SNWolgaK_5" || slot_name == "SNWolgaK_6" || slot_name == "SNWolgaK_7" || slot_name == "SNWolgaK_8" || slot_name == "SNWolgaK_9" || slot_name == "SNWolgaK_10" )
+				if ( slot_name == "GlowPlug" || slot_name == "SparkPlug" || slot_name == "CarBattery" || slot_name == "TruckBattery" || slot_name == "SNWolgaK_1" || slot_name == "SNWolgaK_2" || slot_name == "SNWolgaK_3" || slot_name == "SNWolgaK_4" || slot_name == "SNWolgaK_5" || slot_name == "SNWolgaK_6" || slot_name == "SNWolgaK_7" || slot_name == "SNWolgaK_8" || slot_name == "SNWolgaK_9" || slot_name == "SNWolgaK_10" || slot_name == "SNNivaK_1" || slot_name == "SNNivaK_2" || slot_name == "SNNivaK_3" || slot_name == "SNNivaK_4" || slot_name == "SNNivaK_5" || slot_name == "SNNivaK_6" || slot_name == "SNNivaK_7" || slot_name == "SNNivaK_8" || slot_name == "SNNivaK_9" || slot_name == "SNNivaK_10" || slot_name == "SNSkoda120K_1" || slot_name == "SNSkoda120K_2" || slot_name == "SNSkoda120K_3" || slot_name == "SNSkoda120K_4" || slot_name == "SNSkoda120K_5" || slot_name == "SNSkoda120K_6" || slot_name == "SNSkoda120K_7" || slot_name == "SNSkoda120K_8" || slot_name == "SNSkoda120K_9" || slot_name == "SNSkoda120K_10" || slot_name == "SNZil130K_1" || slot_name == "SNZil130K_2" || slot_name == "SNZil130K_3" || slot_name == "SNZil130K_4" || slot_name == "SNZil130K_5" || slot_name == "SNZil130K_6" || slot_name == "SNZil130K_7" || slot_name == "SNZil130K_8" || slot_name == "SNZil130K_9" || slot_name == "SNZil130K_10" )
 					EngineStop();
 			}
 			
@@ -62,6 +63,7 @@ modded class CarScript
 			CheckVitalItem( IsVitalSparkPlug(), "SparkPlug" );
 			CheckVitalItem( IsVitalGlowPlug(), "GlowPlug" );
 			// Wolga
+			
 			CheckVitalItem( IsVitalKeyWolga1(), "SNWolgaK_1" );
 			CheckVitalItem( IsVitalKeyWolga2(), "SNWolgaK_2" );
 			CheckVitalItem( IsVitalKeyWolga3(), "SNWolgaK_3" );
@@ -73,6 +75,7 @@ modded class CarScript
 			CheckVitalItem( IsVitalKeyWolga9(), "SNWolgaK_9" );
 			CheckVitalItem( IsVitalKeyWolga10(), "SNWolgaK_10" );
 			// Niva
+			
 			CheckVitalItem( IsVitalKeyKeyNiva1(), "SNNivaK_1" );
 			CheckVitalItem( IsVitalKeyKeyNiva2(), "SNNivaK_2" );
 			CheckVitalItem( IsVitalKeyKeyNiva3(), "SNNivaK_3" );
@@ -84,6 +87,7 @@ modded class CarScript
 			CheckVitalItem( IsVitalKeyKeyNiva9(), "SNNivaK_9" );
 			CheckVitalItem( IsVitalKeyKeyNiva10(), "SNNivaK_10" );
 			// Skoda 120
+			
 			CheckVitalItem( IsVitalKeySkoda1(), "SNSkoda120K_1" );
 			CheckVitalItem( IsVitalKeySkoda2(), "SNSkoda120K_2" );
 			CheckVitalItem( IsVitalKeySkoda3(), "SNSkoda120K_3" );
@@ -96,11 +100,88 @@ modded class CarScript
 			CheckVitalItem( IsVitalKeySkoda10(), "SNSkoda120K_10" );
 			
 			// Golf 2
+			CheckVitalItem( IsVitalKeyGolf1(), "SNGolf2K_1" );
+			CheckVitalItem( IsVitalKeyGolf2(), "SNGolf2K_2" );
+			CheckVitalItem( IsVitalKeyGolf3(), "SNGolf2K_3" );
+			CheckVitalItem( IsVitalKeyGolf4(), "SNGolf2K_4" );
+			CheckVitalItem( IsVitalKeyGolf5(), "SNGolf2K_5" );
+			CheckVitalItem( IsVitalKeyGolf6(), "SNGolf2K_6" );
+			CheckVitalItem( IsVitalKeyGolf7(), "SNGolf2K_7" );
+			CheckVitalItem( IsVitalKeyGolf8(), "SNGolf2K_8" );
+			CheckVitalItem( IsVitalKeyGolf9(), "SNGolf2K_9" );
+			CheckVitalItem( IsVitalKeyGolf10(), "SNGolf2K_10" );
+			
+			//Zil130
+			CheckVitalItem( IsVitalKeyZil1(), "SNZil130K_1" );
+			CheckVitalItem( IsVitalKeyZil2(), "SNZil130K_2" );
+			CheckVitalItem( IsVitalKeyZil3(), "SNZil130K_3" );
+			CheckVitalItem( IsVitalKeyZil4(), "SNZil130K_4" );
+			CheckVitalItem( IsVitalKeyZil5(), "SNZil130K_5" );
+			CheckVitalItem( IsVitalKeyZil6(), "SNZil130K_6" );
+			CheckVitalItem( IsVitalKeyZil7(), "SNZil130K_7" );
+			CheckVitalItem( IsVitalKeyZil8(), "SNZil130K_8" );
+			CheckVitalItem( IsVitalKeyZil9(), "SNZil130K_9" );
+			CheckVitalItem( IsVitalKeyZil10(), "SNZil130K_10" );
 			
 			// engine belt is not needed right now
 			//CheckVitalItem( IsVitalEngineBelt(), "EngineBelt" );
 		}
 	}
+	bool IsVitalKeyZil1()
+	{
+		return false
+	}
+	
+	bool IsVitalKeyGolf1()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf2()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf3()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf4()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf5()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf6()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf7()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf8()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf9()
+	{
+		return false;
+	}
+	
+	bool IsVitalKeyGolf10()
+	{
+		return false;
+	}
+	
 	bool IsVitalKeySkoda1()
 	{
 		return false;
